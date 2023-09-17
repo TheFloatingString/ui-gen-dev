bb463bcf862e684c8c61598f2ae96ea474c6627b
James Liang <86000097+jamesliangg@users.noreply.github.com>
2023-09-17 10:46:50 -0400

    Create create_component_draggable.js

---
+++ backend/create_component/create_component_draggable.js
@@ -0,0 +1,136 @@
+// const fs = require('fs');
+import fs from 'fs';
+import express from 'express';
+const app = express();
+import bodyParser from "body-parser";
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const filePath = 'created_component_draggable.jsx';
+const imports = 'import Draggable, { DraggableCore } from "react-draggable"\nimport { Typography, Button, RadioGroup, FormControl, FormLabel, FormControlLabel, Radio } from "@mui/material"\nimport Image from "next/image"'
+const function_header = 'export default function GeneratedComponent() {\n\t<>'
+
+function write(filePath, text) {
+    try {
+        if (fs.existsSync(filePath)) {
+            fs.appendFileSync(filePath, text);
+        }
+        else {
+            fs.writeFileSync(filePath, text);
+        }
+    }
+    catch {
+        console.log("Something went wrong!");
+    }
+}
+
+// create_component(test_json);
+
+function create_component(test_json) {
+    let component_code=imports+"\n\n" + function_header + "\n"
+    let styling=""
+
+    // console.log(Object.keys(test_json));
+    for (var i = 0; i < Object.values(test_json).length; i++) {
+        const styling_name = Object.keys(test_json)[i] + "Styles"
+
+        // this switch statement will write to the file depending on what the React component type is
+        switch (Object.values(test_json)[i].type) {
+            case "Typography":
+                component_code += `<Draggable>\n`
+                component_code=component_code+`\t\t<Typography sx={{ ${styling_name} }}>${Object.values(test_json)[i].text}</Typography>\n`
+                component_code += `</Draggable>\n`
+                const width = `${Object.values(test_json)[i].width}%`
+                const height = `${Object.values(test_json)[i].height}%`
+                const marginTop = `${Object.values(test_json)[i].top_margin}%`
+                const marginLeft = `${Object.values(test_json)[i].left_margin}%`
+                const fontSize = `${Object.values(test_json)[i].font_size}`
+
+                styling=styling+`const ${styling_name} = {\n\twidth: "${width}",\n\theight: "${height}",\n\tmarginTop: "${marginTop}",\n\tmarginRight: "${marginLeft}",\n\tfontSize: ${fontSize}\n}\t\n`
+                break;
+
+            case "Image":
+                component_code += `<Draggable>\n`
+
+                const src = `${Object.values(test_json)[i].src}`
+                const alt = `${Object.keys(test_json)[i]}`
+                const iwidth = `${Object.values(test_json)[i].width}`
+                const iheight = `${Object.values(test_json)[i].height}`
+                const imarginTop = `${Object.values(test_json)[i].top_margin}%`
+                const imarginLeft = `${Object.values(test_json)[i].left_margin}%`
+                styling=styling+`const ${styling_name} = {\n\twidth: "${iwidth}$",\n\theight: "${iheight}%",\n\tmarginTop: "${imarginTop}",\n\tmarginRight: "${imarginLeft}"\n}\n`
+
+                component_code=component_code+`\t\t<Image src="${src}"\n\t\t\twidth={512}\n\t\t\theight={512} \n\t\t\talt="${alt}" \n\t\t\tstyle={ {${styling_name}} }/>\n`
+                component_code += `</Draggable>\n`
+                break;
+
+            case "Button":
+                component_code += `<Draggable>\n`
+                component_code=component_code+`\t\t<Button sx={{ ${styling_name} }}>${Object.values(test_json)[i].text}</Button>\n`
+                component_code += `</Draggable>\n`
+                const bwidth = `${Object.values(test_json)[i].width}%`
+                const bheight = `${Object.values(test_json)[i].height}%`
+                const bmarginTop = `${Object.values(test_json)[i].top_margin}%`
+                const bmarginLeft = `${Object.values(test_json)[i].left_margin}%`
+                const bfontSize = `${Object.values(test_json)[i].font_size}`
+
+                styling=styling+`const ${styling_name} = {\n\twidth: "${bwidth}",\n\theight: "${bheight}",\n\tmarginTop: "${bmarginTop}",\n\tmarginRight: "${bmarginLeft}",\n\tfontSize: ${bfontSize}\n}\n`
+                break;
+
+            case "RadioGroup":
+                component_code += `<Draggable>\n`
+                const rtitle = `${Object.values(test_json)[i].title}`
+                let roptions = `${Object.values(test_json)[i].options}`
+                const rwidth = `${Object.values(test_json)[i].width}%`
+                const rheight = `${Object.values(test_json)[i].height}%`
+                const rmarginTop = `${Object.values(test_json)[i].top_margin}%`
+                const rmarginLeft = `${Object.values(test_json)[i].left_margin}%`
+                component_code=component_code+`\t\t<FormControl sx={{ ${styling_name} }}>\n\t\t\t<FormLabel id="${styling_name}">${rtitle}</FormLabel>\n\t\t\t<RadioGroup\n\t\t\t\taria-labelledby="demo-${styling_name}"\n\t\t\t\tdefaultValue=""\n\t\t\t\tname="controlled-${styling_name}"\n\t\t\t>`
+                roptions = roptions.replace("[", "").replace("]", "").split(",")
+                // loop through the children in the options array and generate a Radio button each
+                for(var x = 0; x < roptions.length; x++)
+                    component_code=component_code+`\n\t\t\t\t<FormControlLabel value="${roptions[x]}" control={<Radio />} label="${roptions[x]}" />`
+                component_code=component_code+"\n\t\t\t</RadioGroup>\n\t\t</FormControl>"
+                styling=styling+`const ${styling_name} = {\n\twidth: "${rwidth}",\n\theight: "${rheight}",\n\tmarginTop: "${rmarginTop}",\n\tmarginRight: "${rmarginLeft}"\n}`
+                component_code += `</Draggable>\n`
+                break;
+
+        }
+    };
+    write(filePath, component_code);
+    write(filePath, "\n\t</>\n}\n");
+    write(filePath, "\n"+styling);
+}
+
+app.post('/api/endpoint', async function (req, res) {
+    try {
+        // const data = req.body;
+        // const action = req.body.action;
+        write("react.json", JSON.stringify(req.body.data));
+        const response_text = JSON.stringify({
+            "result": "Success"
+        });
+        create_component(req.body.data)
+        console.log(response_text);
+        res.send(response_text);
+    }
+    catch {
+        console.log("Error");
+        const response_text = JSON.stringify({
+            "result": "Error"
+        });
+        res.send(response_text);
+    }
+
+
+});
+
+app.get('/', function(req, res, next) {
+    res.send('Greetings!');
+});
+
+app.listen(port, function() {
+    console.log('Server listening at http://CONTAINER_IP_ADDRESS:' + port +'/api/endpoint');
+});
